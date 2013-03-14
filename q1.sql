d804b3e86bf4fc28beef547f110acd75744ffcf4
Vinyas Kedigehalli <vinyas.rk@gmail.com>
2013-03-14 15:47:57 -0700

    ..

--- q1.sql
+++
@@ -1,6 +0,0 @@
-USE VideoSharing;
-
-SELECT advertisements.ad_id AS advertisementID,users.name, users.email
-FROM advertisements INNER JOIN users
-ON advertisements.advertiser = users.username
-WHERE (1000*advertisements.price)/advertisements.views_wanted <0.07;
--- q2.sql
+++
@@ -1,6 +0,0 @@
-USE VideoSharing;
-
-SELECT ad_target_categories.category, sum(advertisements.price) AS category_price
-FROM advertisements INNER JOIN ad_target_categories
-ON advertisements.ad_id = ad_target_categories.ad_id
-GROUP BY ad_target_categories.category;
